b722bb2a952d1f766f89895e446e0e43be73cd9b
Akira HIRATA <hirataakira6@gmail.com>
2022-07-21 10:39:34 +0900

    Update Twitter.vue

--- src/components/Button/Twitter.vue
+++ src/components/Button/Twitter.vue
@@ -12,7 +12,7 @@ export default {
   name: 'Twitter',
   methods:{
     tweet() {
-      window.open(`https://twitter.com/intent/tweet?url=${this.$store.state.tab.url}&text=${this.$store.state.tab.title}`);
+      window.open(`https://twitter.com/intent/tweet?text=${this.$store.state.tab.title}&url=${this.$store.state.tab.url}`);
     }
   }
 }
